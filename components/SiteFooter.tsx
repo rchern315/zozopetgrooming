20ec4a655a407f355efc225fe78f1ab8ffb49e64
Robin Chernak <robinchernak@gmail.com>
2026-09-25 10:44:44 -0700

    Use approved Zozo logo in footer

--- components/SiteFooter.tsx
+++ components/SiteFooter.tsx
@@ -2,8 +2,7 @@ export default function SiteFooter() {
   return (
     <footer className="footer" id="contact">
       <a className="brand brand-lockup footer-brand" href="/" aria-label="Zozo's Pet Grooming home">
-        <span className="brand-badge" aria-hidden="true"><i className="fa-solid fa-dog" /></span>
-        <span className="brand-copy"><strong>ZOZO’S</strong><small>PET GROOMING</small></span>
+        <img className="brand-logo footer-logo" src="/logo.png" alt="Zozo’s Pet Grooming" />
       </a>
       <a href="tel:+17754692228"><i className="fa-solid fa-phone" aria-hidden="true" /> <b>775-469-2228</b><br /><small>Call for appointments</small></a>
       <a href="https://www.instagram.com/zozo_loki/" target="_blank" rel="noreferrer"><i className="fa-brands fa-instagram" aria-hidden="true" /> <b>@ZOZO_LOKI</b><br /><small>Follow on Instagram</small></a>
